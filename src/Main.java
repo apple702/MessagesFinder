782d42663b671c5dfaf36a025f3e5d49405c5129
apple702 <andyyyz2014@gmail.com>
2019-06-02 14:29:34 -0400

    main frame

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,28 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        //TODO read input
+
+
+        int searchTimes = 0;
+        int numberOfMatch = 0;
+        Scanner in = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Please type words");
+
+            String input = in.nextLine();
+
+            if (input.equals("-1")) {
+                break;
+            }
+            //TODO search
+
+        }
+
+        System.out.println("Summary: "  + searchTimes + " searches performed, " + numberOfMatch +  " words matched across all input files");
+
     }
 }
