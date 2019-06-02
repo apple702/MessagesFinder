d955468aba434044de962b333a0249c0b6b4a9f8
apple702 <andyyyz2014@gmail.com>
2019-06-02 15:23:40 -0400

    add method readFile

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.io.*;
 
 public class Main {
 
@@ -11,7 +12,7 @@ public class Main {
         Scanner in = new Scanner(System.in);
 
         while (true) {
-            System.out.println("Please type words");
+            System.out.println("Please type words. Type -1 to exit.");
 
             String input = in.nextLine();
 
@@ -23,6 +24,39 @@ public class Main {
         }
 
         System.out.println("Summary: "  + searchTimes + " searches performed, " + numberOfMatch +  " words matched across all input files");
+
+    }
+
+    public List<String> readFile(String fileName) {
+        File file = new File(fileName);
+        List<String> list = new ArrayList<>();
+        StringBuilder sb = new StringBuilder();
+
+        try {
+            Reader reader = new InputStreamReader(new FileInputStream(file));
+            int tempChar;
+
+            while (true) {
+                if ((tempChar = reader.read()) == -1) {
+                    list.add(sb.toString());
+                    break;
+                }
+
+                if ((char)tempChar == '\n') {
+                    list.add(sb.toString());
+                    sb = new StringBuilder();
+                }
+                else {
+                    sb.append((char)tempChar);
+                }
+            }
+
+            reader.close();
+            return list;
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
