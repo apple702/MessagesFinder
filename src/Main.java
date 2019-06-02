523648087307f490a3d552821d2defc447cc0b10
apple702 <xiao.yue94@outlook.com>
2019-06-02 15:24:44 -0400

    read input names

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,37 @@
+import java.io.*;
 import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        //TODO read input
-
 
         int searchTimes = 0;
         int numberOfMatch = 0;
         Scanner in = new Scanner(System.in);
+        System.out.println("Please type file names:");
+        String path = in.nextLine();
+        File file = new File(path);
+        ArrayList<String> fileNames = new ArrayList<>();
+
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(file));
+            String fileName = null;
+            while ((fileName = reader.readLine()) != null) {
+                fileName = fileName.trim();
+                fileNames.add(fileName);
+            }
+
+            reader.close();
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
 
         while (true) {
-            System.out.println("Please type words");
+            System.out.println("Please type a word,  \"- 1\" to exit:");
 
             String input = in.nextLine();
 
@@ -22,7 +42,9 @@ public class Main {
 
         }
 
-        System.out.println("Summary: "  + searchTimes + " searches performed, " + numberOfMatch +  " words matched across all input files");
+        System.out.println("Summary: " + searchTimes + " searches performed, " + numberOfMatch + " words matched across all input files");
 
     }
 }
+
+
